5c8140f438e955561e5c5192be444c8edf231d02
Dmitriy Snitsaruk <norzes@mail.ru>
2020-06-26 17:01:45 +0400

    Added project structure

---
+++ ShellMeter/Presenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShellMeter.BL;
+
+namespace ShellMeter
+{
+    class Presenter
+    {
+        IMainWindow mainWindow;
+        IDevice device;
+
+        public Presenter(IMainWindow window, IDevice device)
+        {
+            mainWindow = window;
+            this.device = device;
+        }
+    }
+}
